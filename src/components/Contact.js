6e1336aed6030d44fb018dfeddb285b5b28cb8c2
iamzr <iamziarehman@gmail.com>
2023-07-02 20:05:06 +0100

    Updated packages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,14 +29,16 @@ function Contact() {
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9932.669988288171!2d-0.085942!3d51.510143!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760353dc103d0f%3A0xd91a16af73744cc3!2sMonument%20to%20the%20Great%20Fire%20of%20London!5e0!3m2!1sen!2suk!4v1688158033231!5m2!1sen!2suk"
-            width="600"
-            height="450"
-            style="border:0;"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
+          />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             {/* <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
